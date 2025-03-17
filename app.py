eee1bd54ab41c3e2806d48eaa4c75052e1006d95
jiangyh <1162242058@qq.com>
2025-03-17 10:46:15 +0800

    初始化目录

--- app.py
+++ app.py
@@ -8,5 +8,4 @@ if __name__ == '__main__':
     app = QApplication()
     # 默认以动画形式启动应用，如不需要可传入 animate_on_startup=False
     controller = ControllerMain(animate_on_startup=True)
-
     sys.exit(app.exec())
